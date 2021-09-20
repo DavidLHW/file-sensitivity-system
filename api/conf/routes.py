f674693bcbfe3efe29131517eaefcf69267feee1
DavidLHW <davidlin136@gmail.com>
2021-09-20 23:39:12 +0800

    updated routes to accommodate file upload

--- api/conf/routes.py
+++ api/conf/routes.py
@@ -1,16 +1,11 @@
 from flask_restful import Api
 
+from api.handlers.FileHandlers import UploadFile
 from api.handlers.UserHandlers import (
-    DataAdminRequired,
-    DataUserRequired,
     Index,
     Login,
     Logout,
-    RefreshToken,
-    Register,
-    ResetPassword,
-    UsersData,
-    DataSuperAdminRequired)
+    Register)
 
 
 def generate_routes(app):
@@ -31,20 +26,5 @@ def generate_routes(app):
     # Logout page.
     api.add_resource(Logout, "/v1/auth/logout")
 
-    # Refresh page.
-    api.add_resource(RefreshToken, "/v1/auth/refresh")
-
-    # Password reset page. Not forgot.
-    api.add_resource(ResetPassword, "/v1/auth/password_reset")
-
-    # Example user handler for user permission.
-    api.add_resource(DataUserRequired, "/data_user")
-
-    # Example admin handler for admin permission.
-    api.add_resource(DataAdminRequired, "/data_admin")
-
-    # Example user handler for user permission.
-    api.add_resource(DataSuperAdminRequired, "/data_super_admin")
-
     # Get users page with admin permissions.
-    api.add_resource(UsersData, "/users")
+    api.add_resource(UploadFile, "/v1/upload")
