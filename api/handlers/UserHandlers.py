f0e1b115fec8b6b1ba73d542694f21635db8e0a1
DavidLHW <davidlin136@gmail.com>
2021-09-20 23:40:17 +0800

    updated user handlers

--- api/handlers/UserHandlers.py
+++ api/handlers/UserHandlers.py
@@ -1,21 +1,19 @@
 import logging
 from datetime import datetime
 
-from flask import g, request
+from flask import request
 from flask_restful import Resource
 
 import api.error.errors as error
 from api.conf.auth import auth, refresh_jwt
-from api.database.database import db
-from api.models.models import Blacklist, User
-from api.roles import role_required
-from api.schemas.schemas import UserSchema
+from api.database.database import db_session
+from api.models.models import User, Blacklist
 
 
 class Index(Resource):
     @staticmethod
     def get():
-        return "Hello Flask Restful Example!"
+        return "File Sensitivity System"
 
 
 class Register(Resource):
@@ -41,8 +39,11 @@ class Register(Resource):
         if username is None or password is None or email is None:
             return error.INVALID_INPUT_422
 
+        # # Initialise DB session.
+        # db_session = Session()
+
         # Get user if it is existed.
-        user = User.query.filter_by(email=email).first()
+        user = db_session.query(User).filter_by(email=email).first()
 
         # Check if user is existed.
         if user is not None:
@@ -52,10 +53,13 @@ class Register(Resource):
         user = User(username=username, password=password, email=email)
 
         # Add user to session.
-        db.session.add(user)
+        db_session.add(user)
 
         # Commit session.
-        db.session.commit()
+        db_session.commit()
+
+        # Close DB session to prevent memory leaks.
+        db_session.close()
 
         # Return success if registration is completed.
         return {"status": "registration completed."}
@@ -84,8 +88,14 @@ class Login(Resource):
         if email is None or password is None:
             return error.INVALID_INPUT_422
 
+        # # Initialise DB session.
+        # db_session = Session()
+
         # Get user if it is existed.
-        user = User.query.filter_by(email=email, password=password).first()
+        user = db_session.query(User).filter_by(email=email, password=password).first()
+
+        # Close DB session to prevent memory leaks.
+        db_session.close()
 
         # Check if user is not existed.
         if user is None:
@@ -129,8 +139,11 @@ class Logout(Resource):
         # Get refresh token.
         refresh_token = request.json.get("refresh_token")
 
-        # Get if the refresh token is in blacklist
-        ref = Blacklist.query.filter_by(refresh_token=refresh_token).first()
+        # # Initialise DB session.
+        # db_session = Session()
+
+        # Get if the refresh token is in blacklist.
+        ref = db_session.query(Blacklist).filter_by(refresh_token=refresh_token).first()
 
         # Check refresh token is existed.
         if ref is not None:
@@ -140,10 +153,13 @@ class Logout(Resource):
         blacklist_refresh_token = Blacklist(refresh_token=refresh_token)
 
         # Add refresh token to session.
-        db.session.add(blacklist_refresh_token)
+        db_session.add(blacklist_refresh_token)
 
         # Commit session.
-        db.session.commit()
+        db_session.commit()
+
+        # Close DB session to prevent memory leaks.
+        db_session.close()
 
         # Return status of refresh token.
         return {"status": "invalidated", "refresh_token": refresh_token}
@@ -156,8 +172,14 @@ class RefreshToken(Resource):
         # Get refresh token.
         refresh_token = request.json.get("refresh_token")
 
+        # # Initialise DB session.
+        # db_session = Session()
+
         # Get if the refresh token is in blacklist.
-        ref = Blacklist.query.filter_by(refresh_token=refresh_token).first()
+        ref = db_session.query(Blacklist).filter_by(refresh_token=refresh_token).first()
+
+        # Close DB session to prevent memory leaks.
+        db_session.close()
 
         # Check refresh token is existed.
         if ref is not None:
@@ -185,109 +207,4 @@ class RefreshToken(Resource):
         # Return new access token.
         return {"access_token": token}
 
-
-class ResetPassword(Resource):
-    @auth.login_required
-    def post(self):
-
-        # Get old and new passwords.
-        old_pass, new_pass = request.json.get("old_pass"), request.json.get("new_pass")
-
-        # Get user. g.user generates email address cause we put email address to g.user in models.py.
-        user = User.query.filter_by(email=g.user).first()
-
-        # Check if user password does not match with old password.
-        if user.password != old_pass:
-
-            # Return does not match status.
-            return {"status": "old password does not match."}
-
-        # Update password.
-        user.password = new_pass
-
-        # Commit session.
-        db.session.commit()
-
-        # Return success status.
-        return {"status": "password changed."}
-
-
-class UsersData(Resource):
-    @auth.login_required
-    @role_required.permission(2)
-    def get(self):
-        try:
-
-            # Get usernames.
-            usernames = (
-                []
-                if request.args.get("usernames") is None
-                else request.args.get("usernames").split(",")
-            )
-
-            # Get emails.
-            emails = (
-                []
-                if request.args.get("emails") is None
-                else request.args.get("emails").split(",")
-            )
-
-            # Get start date.
-            start_date = datetime.strptime(request.args.get("start_date"), "%d.%m.%Y")
-
-            # Get end date.
-            end_date = datetime.strptime(request.args.get("end_date"), "%d.%m.%Y")
-
-            print(usernames, emails, start_date, end_date)
-
-            # Filter users by usernames, emails and range of date.
-            users = (
-                User.query.filter(User.username.in_(usernames))
-                .filter(User.email.in_(emails))
-                .filter(User.created.between(start_date, end_date))
-                .all()
-            )
-
-            # Create user schema for serializing.
-            user_schema = UserSchema(many=True)
-
-            # Get json data
-            data, errors = user_schema.dump(users)
-
-            # Return json data from db.
-            return data
-
-        except Exception as why:
-
-            # Log the error.
-            logging.error(why)
-
-            # Return error.
-            return error.INVALID_INPUT_422
-
-
 # auth.login_required: Auth is necessary for this handler.
-# role_required.permission: Role required user=0, admin=1 and super admin=2.
-
-
-class DataUserRequired(Resource):
-    @auth.login_required
-    def get(self):
-
-        return "Test user data."
-
-
-class DataAdminRequired(Resource):
-    @auth.login_required
-    @role_required.permission(1)
-    def get(self):
-
-        return "Test admin data."
-
-
-class DataSuperAdminRequired(Resource):
-    @auth.login_required
-    @role_required.permission(2)
-    def get(self):
-
-        return "Test super admin data."
