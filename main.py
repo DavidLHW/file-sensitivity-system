140588cdefc1fc4438fc10116e3efc55fcf5cdc9
DavidLHW <davidlin136@gmail.com>
2021-09-20 23:41:10 +0800

    updated main

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import os
 
 from flask import Flask
 
-from api.conf.config import SQLALCHEMY_DATABASE_URI
+# from api.conf.config import SQLALCHEMY_DATABASE_URI
 from api.conf.routes import generate_routes
-from api.database.database import db
-from api.db_initialiser.db_initialiser import (create_admin_user,
-                                               create_super_admin,
-                                               create_test_user)
+from api.database.database import create_database, recreate_database
+from api.db_initialiser.db_initialiser import create_test_user
 
 
 def create_app():
@@ -15,37 +13,31 @@ def create_app():
     # Create a flask app.
     app = Flask(__name__)
 
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+    UPLOAD_FOLDER = os.path.join(APP_ROOT, "/files")
+
+    print(UPLOAD_FOLDER)
+
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
     # Set debug true for catching the errors.
     app.config['DEBUG'] = True
 
-    # Set database url.
-    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    # allow max 500MB of file size.
+    app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+
+    app.config['UPLOAD_EXTENSIONS'] = ['.txt']
 
     # Generate routes.
     generate_routes(app)
 
-    # Database initialize with app.
-    db.init_app(app)
+    # Create and initialise database if database don't already exist
+    create_database()
 
-    # Check if there is no database.
-    if not os.path.exists(SQLALCHEMY_DATABASE_URI):
-
-        # New db app if no database.
-        db.app = app
-
-        # Create all database tables.
-        db.create_all()
-
-        # Create default super admin user in database.
-        create_super_admin()
-
-        # Create default admin user in database.
-        create_admin_user()
-
-        # Create default test user in database.
-        create_test_user()
+    # Create default test user in database.
+    create_test_user()
 
     # Return app.
     return app
