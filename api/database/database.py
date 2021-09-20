d8e046f035a00f0f5a4af829ed8213887ac5ca90
DavidLHW <davidlin136@gmail.com>
2021-09-20 23:39:38 +0800

    updated data base to utilise sqlalchemy.orm

--- api/database/database.py
+++ api/database/database.py
@@ -1,4 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# Create sql alchemy database object.
-db = SQLAlchemy()
+from api.conf.config import DATABASE_URI
+from api.models.models import Base
+
+
+# Engine object.
+engine = create_engine(DATABASE_URI)
+
+# Session object, not initialised.
+Session = sessionmaker(bind=engine)
+
+db_session = Session()
+
+
+def create_database():
+    Base.metadata.create_all(engine)
+
+
+def recreate_database():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
