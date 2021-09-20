14335103419df03e07302deb1a40dc8b4d82fb38
DavidLHW <davidlin136@gmail.com>
2021-09-20 23:35:01 +0800

    updated auth

--- api/conf/auth.py
+++ api/conf/auth.py
@@ -1,11 +1,11 @@
-from flask_httpauth import HTTPTokenAuth
 from itsdangerous import TimedJSONWebSignatureSerializer as JsonWebToken
+from flask_httpauth import HTTPTokenAuth
 
-# JWT creation.
-jwt = JsonWebToken("top secret!", expires_in=3600)
+# create token.
+jwt = JsonWebToken("secret", expires_in=3600)
 
 # Refresh token creation.
-refresh_jwt = JsonWebToken("telelelele", expires_in=7200)
+refresh_jwt = JsonWebToken("secret2", expires_in=7200)
 
 # Auth object creation.
 auth = HTTPTokenAuth("Bearer")
